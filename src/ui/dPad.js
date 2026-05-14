a760a5da557c941908f31c4c97dac983af389fed
Julia <julia021216@gmail.com>
2026-05-14 20:22:34 +0200

    Update dPad.js

--- src/ui/dPad.js
+++ src/ui/dPad.js
@@ -207,7 +207,7 @@ export function createUI(container, socket) {
             <div id="scoreText"
                 style="
                     position: absolute;
-                    bottom: 30vh;
+                    bottom: 10vh;
                     left: 50%;
                     transform: translateX(-50%);
                     color: white;
